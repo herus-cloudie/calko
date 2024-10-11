426785cc97f03fd0552807b4cfec70bdf11b9a98
Amir(Herus) <herus_cloudie@yahoo.com>
2024-10-11 22:13:01 +0330

    change home page to main

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,27 @@
 import Layout from "@/components/layout/Layout"
-import FlatBlog1 from "@/components/sections/FlatBlog1"
-import FlatBrand1 from "@/components/sections/FlatBrand1"
-import FlatContact1 from "@/components/sections/FlatContact1"
-import FlatFeatured1 from "@/components/sections/FlatFeatured1"
-import FlatMeet1 from "@/components/sections/FlatMeet1"
-import FlatRent1 from "@/components/sections/FlatRent1"
-import FlatSale1 from "@/components/sections/FlatSale1"
-import FlatSearchArea1 from "@/components/sections/FlatSearchArea1"
-import Slider1 from "@/components/sections/Slider1"
-import TfSection1 from "@/components/sections/TfSection1"
+import FlatBlog4 from "@/components/sections/FlatBlog4"
+import FlatBrand4 from "@/components/sections/FlatBrand4"
+import FlatContact4 from "@/components/sections/FlatContact4"
+import FlatDiscover4 from "@/components/sections/FlatDiscover4"
+import FlatExplore4 from "@/components/sections/FlatExplore4"
+import FlatMeet4 from "@/components/sections/FlatMeet4"
+import FlatSale4 from "@/components/sections/FlatSale4"
+import FlatSearchToday4 from "@/components/sections/FlatSearchToday4"
+import FlatWhyChoose4 from "@/components/sections/FlatWhyChoose4"
+import Slider4 from "@/components/sections/Slider4"
 export default function Home() {
 
     return (
         <>
-
-            <Layout headerStyle={1} footerStyle={1}>
-                <Slider1 />
-                <FlatBrand1 />
-                <FlatFeatured1 />
-                <FlatSearchArea1 />
-                <FlatSale1 />
-                <FlatRent1 />
-                <FlatMeet1 />
-                <FlatContact1 />
-                <FlatBlog1 />
-                <TfSection1 />
+            <Layout headerStyle={4} footerStyle={1}>
+                <Slider4 />
+                <FlatExplore4 />
+                <FlatDiscover4 />
+                <FlatSale4 />
+                <FlatWhyChoose4 />
+                <FlatContact4 />
+                <FlatBrand4 />
+                <FlatBlog4 />
             </Layout>
         </>
     )
