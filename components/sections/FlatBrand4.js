0daa71bd5a65981d9e990993dd47700a976c723a
Amir(Herus) <herus_cloudie@yahoo.com>
2024-10-11 22:14:54 +0330

    change trusted section

--- components/sections/FlatBrand4.js
+++ components/sections/FlatBrand4.js
@@ -10,7 +10,7 @@ export default function FlatBrand4() {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="title-section center">
-                                <h4 className="fw-8">Trusted by over 150+ major companies</h4>
+                                <h4 className="fw-8">معتمد و همپیمان با پیش از 30 سازمان معتبر کشور</h4>
                             </div>
                             <div className="swiper-container carousel-5">
                                 <Carousel5 />
